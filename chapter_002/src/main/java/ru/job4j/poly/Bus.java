13c5fc33893e153e5778812edebabbb70d3e85ee
Sergey <sergey.se1ove.morozov@gmail.com>
2019-10-31 12:55:02 +0400

    Интерфейс Input[#191372]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void drive() {
+        System.out.println("The bus is going.");
+    }
+
+    @Override
+    public void passengers(int number) {
+        System.out.println(number + " passengers on the bus");
+    }
+
+    @Override
+    public double fill(int gas) {
+        return gas * 46.25;
+    }
+}
