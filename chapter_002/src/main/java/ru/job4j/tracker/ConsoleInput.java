0f7e71691ed952a6a33e23c68cb32ea0008c4ab0
Sergey <sergey.se1ove.morozov@gmail.com>
2019-11-06 10:50:53 +0400

    if-else-throw[#191382]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -18,10 +18,32 @@ public class ConsoleInput implements Input {
 
     @Override
     public int askInt(String question, int max) {
-        int select = askInt(question);
+        String valueStr = this.askStr(question);
+        hasValid(valueStr, max);
+        int select = Integer.valueOf(valueStr);
+        return select;
+    }
+
+    private boolean hasValid(String valueStr, int max) {
+        if (!checkInt(valueStr)) {
+            throw new NumberFormatException();
+        }
+        if (!checkBound(valueStr, max)) {
+            throw new IllegalStateException();
+        }
+        return true;
+    }
+
+    private boolean checkInt(String valueStr) {
+        Integer.valueOf(valueStr);
+        return true;
+    }
+
+    private boolean checkBound(String valueStr, int max) {
+        int select = Integer.valueOf(valueStr);
         if (select < 0 || select >= max) {
             throw new IllegalStateException(String.format("Out of about %s > [0, %s]", select, max));
         }
-        return select;
+        return true;
     }
 }
