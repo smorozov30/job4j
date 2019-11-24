428f08466a79f17420d39a3825c908857fd06d21
Sergey <sergey.se1ove.morozov@gmail.com>
2019-11-24 13:07:29 +0400

    Исправление ошибок checkstyle.

--- chapter_001/src/main/java/ru/job4j/sort/Machine.java
+++ chapter_001/src/main/java/ru/job4j/sort/Machine.java
@@ -3,13 +3,13 @@ package ru.job4j.sort;
 import java.util.Arrays;
 
 public class Machine {
-    private final int[] COINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int chan = money - price;
-        for (int coin : COINS) {
+        for (int coin : coins) {
             while (chan >= coin) {
                 chan -= coin;
                 rsl[size++] = coin;
