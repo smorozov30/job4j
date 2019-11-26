b51c61d13b1b3f94d7d9319c22dd980b1f235541
Sergey <sergey.se1ove.morozov@gmail.com>
2019-11-26 18:03:31 +0400

    Уникальность элементов и сортировка.[#191220]

--- chapter_003/src/main/java/ru/job4j/lambda/Profiles.java
+++ chapter_003/src/main/java/ru/job4j/lambda/Profiles.java
@@ -1,10 +1,16 @@
 package ru.job4j.lambda;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+    }
+    public List<Address> getUniqueList(List<Profile> profiles) {
+        List<Address> list = this.collect(profiles).stream().distinct().collect(Collectors.toList());
+        list.sort(Comparator.comparing(Address::getCity));
+        return list;
     }
 }
--- chapter_003/src/test/java/ru/job4j/lambda/ProfilesTest.java
+++ chapter_003/src/test/java/ru/job4j/lambda/ProfilesTest.java
@@ -1,19 +1,39 @@
 package ru.job4j.lambda;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.TreeSet;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ProfilesTest {
+    private Address first;
+    private Address second;
+    private Address third;
+    private Address fourth;
+    @Before
+    public void beforeTests() {
+        first = new Address("Moscow", "Nova", 10, 65);
+        second = new Address("Astra", "Novaya", 10, 65);
+        third = new Address("Astra", "Novaya", 10, 65);
+        fourth = new Address("Piter", "Gagarina", 10, 65);
+    }
     @Test
     public void whenReturnList5Addresses() {
-        Address first = new Address("Astra", "Novaya", 10, 65);
-        List<Profile> list = Arrays.asList(new Profile(first), new Profile(first), new Profile(first));
+        List<Profile> list = Arrays.asList(new Profile(first), new Profile(second), new Profile(third));
         List<Address> result = new Profiles().collect(list);
         assertThat(result.size(), is(3));
+    }
+    @Test
+    public void whenGetUniqueAndSortedList() {
+        List<Profile> listProfiles = Arrays.asList(new Profile(first), new Profile(second), new Profile(third), new Profile(fourth));
+        List<Address> result = new Profiles().getUniqueList(listProfiles);
+        List<Address> expected = Arrays.asList(second, first, fourth);
+        assertThat(result, is(expected));
     }
 }
