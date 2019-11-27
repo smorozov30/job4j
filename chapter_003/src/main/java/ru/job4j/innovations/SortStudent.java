fe80cf0e24a6c44445e6bd8d7128dbe6cf617cb4
Sergey <sergey.se1ove.morozov@gmail.com>
2019-11-27 20:49:50 +0400

    Stream API улучшения[#191224]

--- chapter_003/src/main/java/ru/job4j/innovations/SortStudent.java
+++ chapter_003/src/main/java/ru/job4j/innovations/SortStudent.java
@@ -6,8 +6,7 @@ import java.util.stream.Stream;
 
 public class SortStudent {
     public List<Student> levelOf(List<Student> students, int bound) {
-        List<Student> withoutNull = students.stream().flatMap(Stream::ofNullable).sorted().collect(Collectors.toList());
-        List<Student> result = withoutNull.stream().takeWhile(student -> (student.getScope() > bound)).collect(Collectors.toList());
-        return result;
+        return students.stream().flatMap(Stream::ofNullable).sorted().takeWhile(
+                student -> (student.getScope() > bound)).collect(Collectors.toList());
     }
 }
