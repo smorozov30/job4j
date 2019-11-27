4f6b497ba26cfa5d0bb44068cd7d67f3eb42566a
Sergey <sergey.se1ove.morozov@gmail.com>
2019-11-27 21:03:37 +0400

    Вопросы. Lambda, Stream API, JDK Features.[#191229]

--- chapter_004/src/main/java/ru/job4j/stream/Count.java
+++ chapter_004/src/main/java/ru/job4j/stream/Count.java
@@ -1,15 +1,9 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Count {
     public int count(List<Integer> data) {
-        List<Integer> onlyEven = data.stream().filter(num -> num % 2 == 0).collect(Collectors.toList());
-        List<Integer> squared = new ArrayList<>();
-        onlyEven.forEach(num -> squared.add((int) Math.pow(num, 2)));
-        var sum = squared.stream().reduce(0, Integer::sum);
-        return sum;
+        return data.stream().filter(num -> num % 2 == 0).map(num -> (int) Math.pow(num, 2)).reduce(0, Integer::sum);
     }
 }
