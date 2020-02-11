33ae31199de2090925b3cb143f600fef431e9830
Sergey <sergey.se1ove.morozov@gmail.com>
2020-02-11 19:46:27 +0400

    JDBC & Log4j2[#204153]

--- chapter_007/src/main/java/SQLStorage.java
+++ chapter_007/src/main/java/SQLStorage.java
@@ -9,9 +9,19 @@ public class SQLStorage {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, username, password);
-            PreparedStatement st = conn.prepareStatement("DELETE FROM users WHERE id=?");
-            st.setInt(1, 4);
+            PreparedStatement st = conn.prepareStatement("create table survey (id int, name VARCHAR(30) );");
+            st.setString(1, "NEW JAVA USER");
+            st.setInt(2, 4);
             st.executeUpdate();
+//            PreparedStatement st = conn.prepareStatement("SELECT * FROM users u WHERE u.id in (?, ?)");
+//            st.setInt(1, 1);
+//            st.setInt(2, 3);
+//            ResultSet rs = st.executeQuery();
+//            while (rs.next()) {
+//                System.out.println(String.format("%s %s", rs.getString("login"), rs.getTimestamp("create_date")));
+//            }
+//            rs.close();
+//            st.close();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
