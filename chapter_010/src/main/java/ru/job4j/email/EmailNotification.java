2592d2b318f14ceba26031fb8cc42668d1ad6be5
Sergey Morozov <morozov.java.job@gmail.com>
2020-12-03 08:28:15 +0400

    ExecutorService рассылка почты. [#204345]

---
+++ chapter_010/src/main/java/ru/job4j/email/EmailNotification.java
@@ -0,0 +1,33 @@
+package ru.job4j.email;
+
+import java.util.concurrent.ExecutorService;
+
+public class EmailNotification {
+    private ExecutorService pool;
+
+    public void emailTo(User user) {
+        this.pool.submit(() -> {
+            String username = user.getUsername();
+            String email = user.getEmail();
+            String subject = String.format("Notification %s to email %s.", username, email);
+            String body = String.format("Add a new event to %s.", username);
+            send(subject, body, email);
+        });
+
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    public void close() {
+        this.pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
