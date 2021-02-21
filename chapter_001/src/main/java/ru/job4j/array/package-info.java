04897f81875782d7541c4596893e3c57bc9a2eba
Sergey Morozov <moroz.future@gmail.com>
2021-02-21 21:14:57 +0400

    added package-info.java.

--- chapter_001/src/main/java/ru/job4j/array/package-info.java
+++ chapter_001/src/main/java/ru/job4j/array/package-info.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 /**
  * Пакет для задач по теме "Массивы"
  *
- * @author Sergey Morozov (karandash.zapiskin@gmail.com)
+ * @author Sergey Morozov (moroz.future@gmail.com)
  * @version 1
  * @since 24.10.2019
  */
